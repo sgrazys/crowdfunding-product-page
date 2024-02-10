514784d35ca37ac8c02015288f116197e856e8e6
sgrazys <s.grazys@gmail.com>
2024-02-10 23:59:04 +0200

    header WIP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Header from './components/Header';
+import Logo from './components/Logo.jsx';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Header>
+				<Logo />
+			</Header>
+		</div>
+	);
 }
 
 export default App;
